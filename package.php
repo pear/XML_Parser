ba344c8c0d32418d9c2120a9c140848de6156801
Stephan Schmidt <schst@php.net>
2004-05-23 17:33:15 +0000

    added script to generate package file
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_Util/trunk@159468 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ package.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * script to automate the generation of the
+ * package.xml file.
+ *
+ * $Id$
+ *
+ * @author      Stephan Schmidt <schst@php-tools.net>
+ * @package     XML_Parser
+ * @subpackage  Tools
+ */
+
+/**
+ * uses PackageFileManager
+ */ 
+require_once 'PEAR/PackageFileManager.php';
+
+/**
+ * current version
+ */
+$version = '0.6.0beta1';
+
+/**
+ * current state
+ */
+$state = 'beta';
+
+/**
+ * release notes
+ */
+$notes = <<<EOT
+- Fixed bug 1438 (namespaces not accepted for isValidName())
+- added optional parameter to replaceEntities() to define the set of entities to replace
+- added optional parameter to attributesToString() to define, whether entities should be replaced
+EOT;
+
+/**
+ * package description
+ */
+$description = <<<EOT
+Selection of methods that are often needed when working with XML documents. Functionality includes creating of attribute lists from arrays, creation of tags, validation of XML names and more.
+EOT;
+
+$package = new PEAR_PackageFileManager();
+
+$result = $package->setOptions(array(
+    'package'           => 'XML_Util',
+    'summary'           => 'XML utility class.',
+    'description'       => $description,
+    'version'           => $version,
+    'state'             => $state,
+    'license'           => 'PHP License',
+    'filelistgenerator' => 'cvs',
+    'ignore'            => array('package.php', 'package.xml'),
+    'notes'             => $notes,
+    'simpleoutput'      => true,
+    'baseinstalldir'    => 'XML',
+    'packagedirectory'  => './',
+    'dir_roles'         => array('docs' => 'doc',
+                                 'examples' => 'doc',
+                                 'tests' => 'test',
+                                 )
+    ));
+
+if (PEAR::isError($result)) {
+    echo $result->getMessage();
+    die();
+}
+
+$package->addMaintainer('schst', 'lead', 'Stephan Schmidt', 'schst@php-tools.net');
+
+$package->addDependency('PEAR', '', 'has', 'pkg', false);
+$package->addDependency('php', '4.2.0', 'ge', 'php', false);
+$package->addDependency('pcre', '', 'has', 'ext', false);
+
+if (isset($_GET['make']) || (isset($_SERVER['argv'][1]) && $_SERVER['argv'][1] == 'make')) {
+    $result = $package->writePackageFile();
+} else {
+    $result = $package->debugPackageFile();
+}
+
+if (PEAR::isError($result)) {
+    echo $result->getMessage();
+    die();
+}
+?>
